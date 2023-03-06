b782177ce99bdd3091e6cb7dde9bc7de2b364d5a
reshma <npreshmanp@gmail.com>
2023-03-06 19:12:59 +0100

    initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  data="your perfect banking partner"
+  data1="enter your ac no"
+ // acno=""  varaible declaration
+ //or
+ acno:any
+ psw:any
+  userDetails:any={//database name
+    1000:{username:"anu",acno:1000,password:"123",balance:0}, //object ,key is account number
+    1001:{username:"amal",acno:1001,password:"123",balance:0},
+    1002:{username:"bipin",acno:1000,password:"123",balance:0},
+    1003:{username:"reshma",acno:1000,password:"123",balance:0},
+    1004:{username:"anu",acno:1000,password:"123",balance:0}
+  }
+  login()
+{
+  //alert("login worked")
+    var acnum=this.acno
+    var userDetails=this.userDetails
+    var psw=this.psw
+
+   if(acnum in userDetails)
+   {
+    if(psw==userDetails[acnum]["password"]){//here acnum is a variable so the not use " "
+       alert("login success")
+    }
+    else{
+      alert("incorrect password")
+    }
+   }
+   else{
+    alert('incorrect acnum')
+   }
+}
+acnoChange(event:any)
+{
+  this.acno=event.target.value
+  console.log(this.acno);
+  
+}
+pswChange(event:any)
+{
+  this.psw=event.target.value
+  console.log(this.psw);
+  
+}
+
+
+
+}
+//methods
