3455cb120a785b09422c9b4838949d2265d5e884
reshma <npreshmanp@gmail.com>
2023-03-06 21:37:48 +0100

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,8 @@ export class LoginComponent {
   data1="enter your ac no"
  // acno=""  varaible declaration
  //or
- acno:any
- psw:any
+//  acno:any
+//  psw:any
   userDetails:any={//database name
     1000:{username:"anu",acno:1000,password:"123",balance:0}, //object ,key is account number
     1001:{username:"amal",acno:1001,password:"123",balance:0},
@@ -19,12 +19,47 @@ export class LoginComponent {
     1003:{username:"reshma",acno:1000,password:"123",balance:0},
     1004:{username:"anu",acno:1000,password:"123",balance:0}
   }
-  login()
-{
-  //alert("login worked")
-    var acnum=this.acno
+//   login()
+// {
+//   //alert("login worked")
+//     var acnum=this.acno
+//     var userDetails=this.userDetails
+//     var psw=this.psw
+
+//    if(acnum in userDetails)
+//    {
+//     if(psw==userDetails[acnum]["password"]){//here acnum is a variable so the not use " "
+//        alert("login success")
+//     }
+//     else{
+//       alert("incorrect password")
+//     }
+//    }
+//    else{
+//     alert('incorrect acnum')
+//    }
+// }
+// acnoChange(event:any)
+// {
+//   this.acno=event.target.value
+//   console.log(this.acno);
+  
+// }
+// pswChange(event:any)
+// {
+//   this.psw=event.target.value
+//   console.log(this.psw);
+  
+// }
+
+login(acnum:any,psw:any)
+{  //console.log(acnum,psw);
+
+//   //alert("login worked")
+    var acnum=acnum.value
+   
+    var psw=psw.value
     var userDetails=this.userDetails
-    var psw=this.psw
 
    if(acnum in userDetails)
    {
@@ -39,20 +74,7 @@ export class LoginComponent {
     alert('incorrect acnum')
    }
 }
-acnoChange(event:any)
-{
-  this.acno=event.target.value
-  console.log(this.acno);
-  
-}
-pswChange(event:any)
-{
-  this.psw=event.target.value
-  console.log(this.psw);
-  
-}
-
-
 
 }
 //methods
+
